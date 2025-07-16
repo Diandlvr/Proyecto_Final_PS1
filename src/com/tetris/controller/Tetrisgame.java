5c10d4247e4553f8a8dfc5c46e95d1a7bd236e7b
juanp <juanxp8879@gmail.com>
2025-07-16 04:11:35 -0500

    Commit inicial: este es el primer commit para ir guardando el proyecto

---
+++ src/com/tetris/controller/Tetrisgame.java
@@ -0,0 +1,15 @@
+package com.tetris.controller;
+//importo la clase tablero para usarlo en esta clase
+import com.tetris.model.Tablero;
+public class Tetrisgame {
+    private Tablero tablero;
+    // TODO: pieza activa, temporizador, puntuación
+    public Tetrisgame() {
+        // TODO: inicializar tablero y temporizador
+    }
+    public void pasoDelJuego() {
+        // TODO: bajar pieza, fijar, limpiar líneas, generar nueva pieza
+    }
+    // TODO: métodos para moverIzquierda, moverDerecha, rotar, caerRápido
+}
+
---
+++ src/com/tetris/model/TipoPieza.java
@@ -0,0 +1,11 @@
+package com.tetris.model;
+//formas de las piezas
+    public enum TipoPieza {
+        I,
+        O,
+        T,
+        S,
+        Z,
+        J,
+        L;
+    }
