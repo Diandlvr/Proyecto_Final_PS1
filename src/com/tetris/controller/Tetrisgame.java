0c90cdd6506ba25601d71749e9f3157673b3e0a6
danahprogrammer <danahrodriguez05@gmail.com>
2025-07-23 13:46:33 -0500

    Update Tetrisgame.java
    Cambios cuando se trabajó parte gráfica, agregamos y cambiamos

--- src/com/tetris/controller/Tetrisgame.java
+++ src/com/tetris/controller/Tetrisgame.java
@@ -1,12 +1,12 @@
 package com.tetris.controller;
 
-import com.tetris.model.Tablero;
 import com.tetris.model.Piezas;
 import com.tetris.model.PiezasFactory;
+import com.tetris.model.Tablero;
 import com.tetris.view.GamePanel;
-
 import java.awt.Point;
 import java.util.Arrays;
+
 
 public class Tetrisgame {
     private Tablero tablero;
@@ -15,26 +15,36 @@ public class Tetrisgame {
     private PiezasFactory fabrica;
     private GamePanel vista;
     private int puntuacion;
+    private int nivel;
+    private int lineasCompletadas;
     private boolean gameOver;
+    private boolean pausado;
+    private int velocidad;
 
 
+    
     public Tetrisgame() {
-        this.tablero     = new Tablero();
-        this.fabrica     = new PiezasFactory();
+        this.tablero = new Tablero();
+        this.fabrica = new PiezasFactory();
         this.piezaActual = fabrica.nuevaPieza();
         this.siguientePieza = fabrica.nuevaPieza();
-        this.puntuacion  = 0;
+        this.puntuacion = 0;
+        this.nivel = 1;
+        this.lineasCompletadas = 0;
         this.gameOver = false;
-
+        this.pausado = false;
+        this.velocidad = 500; // ms entre caídas
     }
 
     // baja la pieza, si no cabe, la bloquea
     public void pasoDelJuego() {
-        if (gameOver) return;
-        if  (!tablero.puedeColocar(piezaActual,0,0)){
+        if (gameOver || pausado) return;
+        
+        if (!tablero.puedeColocar(piezaActual, 0, 0)) {
             gameOver = true;
             return;
         }
+        
         if (tablero.puedeColocar(piezaActual, 0, 1)) {
             piezaActual.mover(0, 1);
         } else {
@@ -42,9 +52,11 @@ public class Tetrisgame {
         }
         vista.repaint();
     }
+
     public void setVista(GamePanel vista) {
         this.vista = vista;
     }
+
     // fija la pieza, limpia líneas y pide nueva
     public void bloquearPieza() {
         for (Point b : piezaActual.getForma()) {
@@ -52,8 +64,29 @@ public class Tetrisgame {
             int y = piezaActual.getPosicion().y + b.y;
             tablero.getCeldas(x, y).ocupar(piezaActual.getColor());
         }
+        
         int lineas = tablero.limpiarLineas();
-        puntuacion += lineas * 100;
+        lineasCompletadas += lineas;
+        
+        // Sistema de puntuación mejorado
+        if (lineas > 0) {
+            int puntos = 0;
+            switch (lineas) {
+                case 1: puntos = 100; break;
+                case 2: puntos = 300; break;
+                case 3: puntos = 500; break;
+                case 4: puntos = 800; break;
+            }
+            puntuacion += puntos * nivel;
+        }
+        
+        // Sistema de niveles
+        int nuevoNivel = (lineasCompletadas / 10) + 1;
+        if (nuevoNivel > nivel) {
+            nivel = nuevoNivel;
+            velocidad = Math.max(50, 500 - (nivel - 1) * 50); // Aumenta velocidad
+        }
+        
         piezaActual = siguientePieza;
         siguientePieza = fabrica.nuevaPieza();
         vista.repaint();
@@ -61,14 +94,14 @@ public class Tetrisgame {
 
     // movimientos laterales
     public void moverIzquierda() {
-        if (tablero.puedeColocar(piezaActual, -1, 0)) {
+        if (!gameOver && !pausado && tablero.puedeColocar(piezaActual, -1, 0)) {
             piezaActual.mover(-1, 0);
             vista.repaint();
         }
     }
 
     public void moverDerecha() {
-        if (tablero.puedeColocar(piezaActual, 1, 0)) {
+        if (!gameOver && !pausado && tablero.puedeColocar(piezaActual, 1, 0)) {
             piezaActual.mover(1, 0);
             vista.repaint();
         }
@@ -76,11 +109,10 @@ public class Tetrisgame {
 
     // "soft drop"
     public void bajarFila() {
-        if (!gameOver) {
-
-
+        if (!gameOver && !pausado) {
             if (tablero.puedeColocar(piezaActual, 0, 1)) {
                 piezaActual.mover(0, 1);
+                puntuacion += 1; // Puntos por soft drop
             } else {
                 bloquearPieza();
             }
@@ -88,11 +120,22 @@ public class Tetrisgame {
         }
     }
 
+    // "hard drop"
+    public void hardDrop() {
+        if (!gameOver && !pausado) {
+            int filasBajadas = 0;
+            while (tablero.puedeColocar(piezaActual, 0, 1)) {
+                piezaActual.mover(0, 1);
+                filasBajadas++;
+            }
+            puntuacion += filasBajadas * 2; // Puntos por hard drop
+            bloquearPieza();
+        }
+    }
+
     // rotación con rollback si choca
     public void rotarPieza() {
-        if (!gameOver) {
-
-
+        if (!gameOver && !pausado) {
             Point[] backup = Arrays.stream(piezaActual.getForma())
                     .map(Point::new)
                     .toArray(Point[]::new);
@@ -104,17 +147,62 @@ public class Tetrisgame {
         }
     }
 
+    // Pausar/reanudar juego
+    public void togglePausa() {
+        if (!gameOver) {
+            pausado = !pausado;
+            vista.repaint();
+        }
+    }
+
+    // Reiniciar juego
+    public void reiniciarJuego() {
+        this.tablero = new Tablero();
+        this.piezaActual = fabrica.nuevaPieza();
+        this.siguientePieza = fabrica.nuevaPieza();
+        this.puntuacion = 0;
+        this.nivel = 1;
+        this.lineasCompletadas = 0;
+        this.gameOver = false;
+        this.pausado = false;
+        this.velocidad = 500;
+        vista.repaint();
+    }
+
     // getters para la vista
-    public Tablero getTablero()      {
-        return tablero; }
-    public Piezas  getPiezaActual()  {
-        return piezaActual; }
+    public Tablero getTablero() {
+        return tablero;
+    }
+    
+    public Piezas getPiezaActual() {
+        return piezaActual;
+    }
+    
     public Piezas getSiguientePieza() {
         return siguientePieza;
     }
-    public int     getPuntuacion()   {
-        return puntuacion; }
-    public boolean isGameOver()  {
+    
+    public int getPuntuacion() {
+        return puntuacion;
+    }
+    
+    public int getNivel() {
+        return nivel;
+    }
+    
+    public int getLineasCompletadas() {
+        return lineasCompletadas;
+    }
+    
+    public boolean isGameOver() {
         return gameOver;
+    }
+    
+    public boolean isPausado() {
+        return pausado;
+    }
+    
+    public int getVelocidad() {
+        return velocidad;
     }
 }
